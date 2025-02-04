c73793321be787c131cec05041e3548f77e962af
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2025-02-04 10:36:46 +0200

    Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -9,7 +9,7 @@ powercfg -change -monitor-timeout-ac 15
 powercfg -change -standby-timeout-ac 0
 powercfg -change -hibernate-timeout-ac 0
 powercfg -change -monitor-timeout-dc 10
-powercfg -change -standby-timeout-dc 0
+powercfg -change -standby-timeout-dc 30
 powercfg -change -hibernate-timeout-dc 30
 timeout 200
 powershell.exe C:\NA\Installation\NIC_Power_Management.ps1
