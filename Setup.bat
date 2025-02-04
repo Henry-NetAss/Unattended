6e80d2dfcbb6dbc9bbf70aa0ec95942fa5aaed1e
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2025-02-04 10:34:56 +0200

    Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,15 +1,17 @@
 cd c:\NA\Installation
 powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 2
 powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 2
-powercfg /setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 2
-powercfg /setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 2
+powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 2
+powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 2
 powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 96996bc0-ad50-47ec-923b-6f41874dd9eb 0
 powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 96996bc0-ad50-47ec-923b-6f41874dd9eb 0
 powercfg -change -monitor-timeout-ac 15
 powercfg -change -standby-timeout-ac 0
 powercfg -change -hibernate-timeout-ac 0
 powercfg -change -monitor-timeout-dc 10
-powercfg -change -standby-timeout-ac 30timeout 300
+powercfg -change -standby-timeout-dc 0
+powercfg -change -hibernate-timeout-dc 30
+timeout 200
 powershell.exe C:\NA\Installation\NIC_Power_Management.ps1
 timeout 100
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
